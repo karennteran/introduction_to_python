70748d5027ad84cb8525c83878c926324d538871
Django0033 <edpu0033@gmail.com>
2021-10-30 14:18:28 -0500

    Cleaning the code as the instructor's example.

--- guess number.py
+++ guess number.py
@@ -9,15 +9,17 @@ while play_again == "yes":
     guess = int(input("What is your guess? "))
     tries = 1
     while guess != number:
+        guess = int(input("What is your guess? "))
+        tries = tries + 1
 
         if guess > number:
             print("Guess lower")
-            guess = int(input("What is your guess? "))
-            tries = tries + 1
+            # guess = int(input("What is your guess? "))
+            # tries = tries + 1
         elif guess < number:
             print("Guess higher")
-            guess = int(input("What is your guess? "))
-            tries = tries + 1
+            # guess = int(input("What is your guess? "))
+            # tries = tries + 1
     print("You guessed it!")
     print(f"You made {tries} tries")
     play_again = input("Do you want to play again? ")
